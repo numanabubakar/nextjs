2ea8578414ce052b3841d6affe127cbac0b3a9eb
Numan Abubakar <bhalonoman@gmail.com>
2023-08-27 18:00:37 +0500

    add link

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Home = () => {
   return (
     <div className='flex flex-1 justify-center	self-center py-12	'>
-
+<div>
+  <Link href='/contact'> Contact</Link>
+</div>
     <div className='px-3'>Hello NextJs! ,</div>
     <div>
 <p>My Name is Numan Abubakar ! Will you be my Best Friend?</p>
